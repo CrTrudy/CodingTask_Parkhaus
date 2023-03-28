e1eb26dbfe32c95fd11e3b3bdd15e74cd27b4904
Samuel Witten <wittensamuel@gmail.com>
2023-03-28 08:37:00 +0200

    initial consoleapp ParkMaster3000

---
+++ ParkMaster3000/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParkMaster3000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
