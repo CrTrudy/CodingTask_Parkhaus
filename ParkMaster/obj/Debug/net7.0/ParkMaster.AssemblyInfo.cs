d6130acd9937fce42b76f383a88add4137e0fcfa
CrTrudy <111459119+CrTrudy@users.noreply.github.com>
2023-03-31 08:03:21 +0200

    ++

--- ParkMaster/obj/Debug/net7.0/ParkMaster.AssemblyInfo.cs
+++ ParkMaster/obj/Debug/net7.0/ParkMaster.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ParkMaster")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("parkmaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ParkMaster")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ParkMaster")]
+[assembly: System.Reflection.AssemblyProductAttribute("parkmaster")]
+[assembly: System.Reflection.AssemblyTitleAttribute("parkmaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
